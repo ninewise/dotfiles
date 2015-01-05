f17942c98e7718457d4b2e6bc3b52d08da3eb556
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-01-05 17:15:23 +0100

    puuuush

---
+++ local/bin/push.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Thanks to noctua for the base script
+# Thanks to Iasoon for the advanced ssh config parsing
+set -e
+
+sshhost="Flesje"
+fles="http://flashyoshi.me"
+
+# Find ssh username
+user=$(
+    # Join config entries
+    sed -n '$!{/host /I!{H;d}};x;s/\n/ /g;p' $HOME/.ssh/config  |
+    # Extract user/host combinations
+    grep -i 'user' |
+    sed 's/^.*host \([^ \t]\+\).*user \([^ \t]\+\).*$/\1 \2/I' |
+    # Find user
+    (grep "^$sshhost" || echo $USER) | cut -d ' ' -f 2 )
+    
+file="$(mktemp XXXXXX.png)"
+scrot $* "$file"
+chmod 755 $file
+scp -p $file $sshhost:~/images/
+rm "$file"
+url="$fles:8080/~$user/images/$file"
+notify-send "Screenshot uploaded to $url"
+echo "$url" | xclip
