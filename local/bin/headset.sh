1bfc329df8090c9ebdcb951019bb52f8021d4c3d
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-04-04 13:57:24 +0200

    bins - silence headset and add stylish-haskell

--- local/bin/headset.sh
+++ local/bin/headset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-if ! pgrep pulseaudio; then pulseaudio --start; fi
-cat <(echo "connect 00:11:67:11:18:9B") <(echo "quit") | bluetoothctl
+if ! pgrep pulseaudio; then pulseaudio --start 2> /dev/null ; fi
+cat <(echo "connect 00:11:67:11:18:9B") <(echo "quit") \
+    | bluetoothctl 2> /dev/null 2>&1
 
