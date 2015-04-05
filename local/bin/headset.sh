89caf40990d02623eb5bc4aca0d04015c141e9cf
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-04-06 01:05:24 +0200

    bins - fail

--- local/bin/headset.sh
+++ local/bin/headset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if ! pgrep pulseaudio; then pulseaudio --start 2> /dev/null ; fi
 cat <(echo "connect 00:11:67:11:18:9B") <(echo "quit") \
-    | bluetoothctl 2> /dev/null 2>&1
+    | bluetoothctl > /dev/null 2>&1
 
