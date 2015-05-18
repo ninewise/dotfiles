b4401860acfcab9804d3d17f436a691ec64ae8ef
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-05-18 14:16:43 +0200

    bins - also put it in the clipboard

--- local/bin/pwcalc.sh
+++ local/bin/pwcalc.sh
@@ -14,5 +14,6 @@ secret="${pair#*|}"
 pass="$(echo -n "$secret$alias_" | shasum | xxd -r -p | base64 | colrm 17)"
 
 echo -n "$pass" | xclip
+echo -n "$pass" | xclip -sel c
 notify-send "xclip set to ${pass:0:3}...${pass:(-3):3}"
 
