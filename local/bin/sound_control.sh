07b33e2de31e770b65ebb46f7e018859f32ef671
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-11-11 00:33:17 +0100

    why would I want output?

--- local/bin/sound_control.sh
+++ local/bin/sound_control.sh
@@ -5,9 +5,9 @@ command="${1:=toggle}"
 current="$(amixer get Master | sed -n '/%/s/.*\[\(.*\)%\].*/\1/p' | head -1)"
 
 case $command in
-    toggle) amixer set Master toggle ;;
-    down)   amixer set Master "$((current - 5))%" ;;
-    up)     amixer set Master "$((current + 5))%" ;;
+    toggle) amixer -q set Master toggle ;;
+    down)   amixer -q set Master "$((current - 5))%" ;;
+    up)     amixer -q set Master "$((current + 5))%" ;;
     *)      notify-send "learn to volume" ;;
 esac
 
