d9711647c4e6ca8ba2cbf5a9f1580a87863444fb
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-04-10 20:09:34 +0200

    no cammie watching, just scrots

--- local/bin/sphere.sh
+++ local/bin/sphere.sh
@@ -5,4 +5,8 @@ case $1 in
     zetel) curl --data "command=set_pos&posX=50&posY=22" kelder.zeus.ugent.be/webcam/cgi/ptdc.cgi ;;
 esac > /dev/null
 
-curl http://kelder.zeus.ugent.be/webcam/video/mjpg.cgi | mpv --no-correct-pts --fps 5 -
+foto="$(mktemp XXXXXX.jpg)"
+curl http://kelder.zeus.ugent.be/webcam/video/mjpg.cgi | ffmpeg -y -ss 0 -i - -vframes 1 -q:v 2 "$foto"
+feh "$foto"
+rm "$foto"
+
