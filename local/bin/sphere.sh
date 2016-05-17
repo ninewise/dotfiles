ddbbfe10b5310365b69ebfcb273f0c329ab6f7ea
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-05-17 14:53:48 +0200

    exes - sphere started to skew after some time

--- local/bin/sphere.sh
+++ local/bin/sphere.sh
@@ -9,6 +9,6 @@ case $1 in
     klein) curl --data "command=set_pos&posX=0&posY=12"  kelder.zeus.ugent.be/webcam/cgi/ptdc.cgi  ;;
     groot) curl --data "command=set_pos&posX=50&posY=10" kelder.zeus.ugent.be/webcam/cgi/ptdc.cgi  ;;
     zetel) curl --data "command=set_pos&posX=50&posY=22" kelder.zeus.ugent.be/webcam/cgi/ptdc.cgi  ;;
-    *)     curl http://kelder.zeus.ugent.be/webcam/video/mjpg.cgi | mpv --no-correct-pts --fps 4.5 - ;;
+    *)     curl http://kelder.zeus.ugent.be/webcam/video/mjpg.cgi | mpv --no-correct-pts --fps 4 - ;;
 esac > /dev/null 2>&1
 
