4bf0b73da6b199153f118a1706d39c6d5ae6a359
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-05-09 08:53:03 +0200

    show sent mails in inbox

--- config/alot/hooks.py
+++ config/alot/hooks.py
@@ -1,6 +1,5 @@
 
 from alot.commands.globals import ExternalCommand
-from alot.commands.search import OpenThreadCommand
 
 def loop_hook(ui=None):
     ui.apply_command(ExternalCommand(
@@ -9,8 +8,4 @@ def loop_hook(ui=None):
     ))
     if ui and ui.current_buffer:
         ui.update()
-
-def post_search_select(ui=None, dbm=None, cmd=None):
-    if isinstance(cmd, OpenThreadCommand):
-        cmd.thread.remove_tags({"unread"})
 
