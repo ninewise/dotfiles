b157ae0c3bc601c9fed47103628d02e22f134559
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2018-10-08 14:00:43 +0200

    why was I using curl there and \ is annoying to type

--- local/bin/sphere.sh
+++ local/bin/sphere.sh
@@ -14,15 +14,15 @@ if [ -n "$1" ]; then
             klein) curl --data "command=set_pos&posX=16&posY=16"          "$cmd_url"  ;;
             groot) curl --data "command=set_pos&posX=50&posY=10"          "$cmd_url"  ;;
             zetel) curl --data "command=set_pos&posX=0&posY=12"           "$cmd_url"  ;;
-            \>)    curl --data "command=set_relative_pos&posX=10&posY=0"  "$cmd_url"  ;;
-            \<)    curl --data "command=set_relative_pos&posX=-10&posY=0" "$cmd_url"  ;;
-            v)     curl --data "command=set_relative_pos&posX=0&posY=-10" "$cmd_url"  ;;
-            \^)    curl --data "command=set_relative_pos&posX=0&posY=10"  "$cmd_url"  ;;
+            r|\>)  curl --data "command=set_relative_pos&posX=10&posY=0"  "$cmd_url"  ;;
+            l|\<)  curl --data "command=set_relative_pos&posX=-10&posY=0" "$cmd_url"  ;;
+            d|v)   curl --data "command=set_relative_pos&posX=0&posY=-10" "$cmd_url"  ;;
+            u|\^)  curl --data "command=set_relative_pos&posX=0&posY=10"  "$cmd_url"  ;;
         esac > /dev/null 2>&1
         shift
     done
 else
-    curl http://kelder.zeus.ugent.be/webcam/video/mjpg.cgi | mpv --no-correct-pts --fps 4 -
+    mpv --no-correct-pts --fps 4 http://kelder.zeus.ugent.be/webcam/video/mjpg.cgi 
 fi
 
 
