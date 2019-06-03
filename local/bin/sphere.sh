41af44e6fc5275cbcb8484c1ad5d657421504e47
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-06-03 10:19:56 +0200

    fix cammie positions

--- local/bin/sphere.sh
+++ local/bin/sphere.sh
@@ -11,13 +11,13 @@ if [ -n "$1" ]; then
                    feh "$foto"
                    rm "$foto"
                    ;;
-            klein) curl --data "command=set_pos&posX=16&posY=16"          "$cmd_url"  ;;
-            groot) curl --data "command=set_pos&posX=50&posY=10"          "$cmd_url"  ;;
-            zetel) curl --data "command=set_pos&posX=0&posY=12"           "$cmd_url"  ;;
+            klein) curl --data "command=set_pos&posX=28&posY=16"          "$cmd_url"  ;;
+            groot) curl --data "command=set_pos&posX=60&posY=6"           "$cmd_url"  ;;
+            zetel) curl --data "command=set_pos&posX=10&posY=12"          "$cmd_url"  ;;
             r|\>)  curl --data "command=set_relative_pos&posX=10&posY=0"  "$cmd_url"  ;;
             l|\<)  curl --data "command=set_relative_pos&posX=-10&posY=0" "$cmd_url"  ;;
-            d|v)   curl --data "command=set_relative_pos&posX=0&posY=-10" "$cmd_url"  ;;
-            u|\^)  curl --data "command=set_relative_pos&posX=0&posY=10"  "$cmd_url"  ;;
+            d|v)   curl --data "command=set_relative_pos&posX=0&posY=-5"  "$cmd_url"  ;;
+            u|\^)  curl --data "command=set_relative_pos&posX=0&posY=5"   "$cmd_url"  ;;
         esac > /dev/null 2>&1
         shift
     done
