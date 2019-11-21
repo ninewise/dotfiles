259680d31a4a6934229885e0188bf54907bfd90d
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-11-21 15:11:38 +0100

    Modify xrandr and audio script to be dock-aware

--- local/bin/sslider.sh
+++ local/bin/sslider.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-uplimit="$(amixer get Master | grep "Limits:" | tr -s ' ' | cut -d' ' -f6)"
-lolimit="$(amixer get Master | grep "Limits:" | tr -s ' ' | cut -d' ' -f4)"
-current="$(amixer get Master | grep "Mono:"   | tr -s ' ' | cut -d' ' -f4)"
+card="$(aplay -l | sed -n 's/^card [0-9]*: \(\w*\).*/\1/p' | tail -1)"
+control="$(amixer -c "$card" scontrols | sed -n "1s/.*'\(.*\)'.*/\1/p")"
+
+uplimit="$(amixer -c "$card" get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f6)"
+lolimit="$(amixer -c "$card" get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f4)"
+current="$(amixer -c "$card" get "$control" | sed -n '/dB/{p;q}' | sed 's/.*Playback \([0-9]*\) .*/\1/')"
 wjt -l "$lolimit" -u "$uplimit" -x "$current" | while read next; do
-    amixer -q set Master "$next"
+    amixer -c "$card" -q set "$control" "$next"
 done
