e071340930ec2fd102879ab9a6567e63b85e4b6c
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-05-15 15:20:12 +0200

    Update alot and a hook to report spam to posteo

--- config/alot/hooks.py
+++ config/alot/hooks.py
@@ -12,14 +12,25 @@ async def spam(ui):
 	message = ui.current_buffer.get_selected_message()
 	message.add_tags(['spam'])
 	message.remove_tags(['unread'])
-	envelope = Envelope(
-		headers=dict(
-			From=['Felix Van der Jeugt <felix.vanderjeugt@ugent.be>'],
-			To=['spam@ugent.be'],
-			Subject=['spam'],
-			References=['<' + message.get_message_id() + '>']),
-		bodytext='spam',
-		tags=['spam'],
-		account=settings.account_matching_address('felix.vanderjeugt@ugent.be'))
+	if 'ugent' in message.get_tags():
+		envelope = Envelope(
+			headers=dict(
+				From=['Felix Van der Jeugt <felix.vanderjeugt@ugent.be>'],
+				To=['spam@ugent.be'],
+				Subject=['spam'],
+				References=['<' + message.get_message_id() + '>']),
+			bodytext='spam',
+			tags=['spam'],
+			account=settings.account_matching_address('felix.vanderjeugt@ugent.be'))
+	else:
+		envelope = Envelope(
+			headers=dict(
+				From=['Felix Van der Jeugt <felix.vanderjeugt@posteo.net>'],
+				To=['spamreport@posteo.de'],
+				Subject=['spam'],
+				References=['<' + message.get_message_id() + '>']),
+			bodytext='',
+			tags=['spam'],
+			account=settings.account_matching_address('felix.vanderjeugt@posteo.net'))
 	envelope.attach(message.get_filename())
 	ui.buffer_open(EnvelopeBuffer(ui, envelope))
