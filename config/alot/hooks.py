a93d6b56ba819b38b4a938b51d60051bde7f989d
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-08-20 11:54:15 +0200

    Remove delays with 0

--- config/alot/hooks.py
+++ config/alot/hooks.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
+from datetime import date, timedelta
+
+from alot.buffers import EnvelopeBuffer
+from alot.db.envelope import Envelope
+from alot.settings.const import settings
 
 async def delay(ui, days):
-	from datetime import date, timedelta
-	datestr = str(date.today() + timedelta(days=days))
-	await ui.apply_commandline(f'toggletags inbox,delay,delay/{datestr}')
+	datetag = 'delay/' + str(date.today() + timedelta(days=days))
+
+	if ui.current_buffer.modename == 'search':
+		messages = ui.current_buffer.get_selected_thread().get_messages().keys()
+	elif ui.current_buffer.modename == 'thread':
+		messages = [ ui.current_buffer.get_selected_message() ]
+
+	if days != 0:
+		for message in messages:
+			if 'inbox' in message.get_tags():
+				message.add_tags(['delay', datetag])
+		await ui.apply_commandline('untag inbox,unread')
+	else:
+		for message in messages:
+			if 'delay' in message.get_tags():
+				message.remove_tags([t for t in message.get_tags() if t.startswith('delay')])
+				message.add_tags(['inbox'])
+		await ui.apply_commandline('untag delay,delay/*')
 
 async def spam(ui):
-	from alot.buffers import EnvelopeBuffer
-	from alot.db.envelope import Envelope
-	from alot.settings.const import settings
 	message = ui.current_buffer.get_selected_message()
 	message.add_tags(['spam'])
-	message.remove_tags(['unread'])
+	message.remove_tags(['unread', 'inbox'])
 	if 'ugent' in message.get_tags():
 		envelope = Envelope(
 			headers=dict(
