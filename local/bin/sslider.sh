fe5db6e793ca27587eadea57b2adadef26f7742f
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-12-04 14:45:07 +0100

    use the default alsa card

--- local/bin/sslider.sh
+++ local/bin/sslider.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-card="$(aplay -l | sed -n 's/^card [0-9]*: \(\w*\).*/\1/p' | tail -1)"
-#card=PCH
-control="$(amixer -c "$card" scontrols | sed -n "1s/.*'\(.*\)'.*/\1/p")"
+control="$(amixer scontrols | sed -n "1s/.*'\(.*\)'.*/\1/p")"
 
-uplimit="$(amixer -c "$card" get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f6)"
-lolimit="$(amixer -c "$card" get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f4)"
-current="$(amixer -c "$card" get "$control" | sed -n '/dB/{p;q}' | sed 's/.*Playback \([0-9]*\) .*/\1/')"
+uplimit="$(amixer get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f6)"
+lolimit="$(amixer get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f4)"
+current="$(amixer get "$control" | sed -n '/dB/{p;q}' | sed 's/.*Playback \([0-9]*\) .*/\1/')"
 wjt -l "$lolimit" -u "$uplimit" -x "$current" | while read next; do
-    amixer -c "$card" -q set "$control" "$next"
+    amixer -q set "$control" "$next"
 done
