1aee0d7eb4fa39e14eb7d533ef99ce11b1fc7f0b
Felix Van der Jeugt <felix.vanderjeugt@sigasi.com>
2022-06-13 17:01:12 +0200

    generalize sslider over both PA and alsa

--- local/bin/sslider.sh
+++ local/bin/sslider.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-control="$(amixer scontrols | sed -n "1s/.*'\(.*\)'.*/\1/p")"
+if pamixer -v >/dev/null; then
+	uplimit=100
+	lolimit=0
+	current="$(pamixer --get-volume)"
+	setV() { pamixer --set-volume "$@"; }
+else
+	control="$(amixer scontrols | sed -n "1s/.*'\(.*\)'.*/\1/p")"
+	uplimit="$(amixer get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f6)"
+	lolimit="$(amixer get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f4)"
+	current="$(amixer get "$control" | sed -n '/dB/{p;q}' | sed 's/.*Playback \([0-9]*\) .*/\1/')"
+	setV() { amixer -q set "$control" "$@"; }
+fi
 
-uplimit="$(amixer get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f6)"
-lolimit="$(amixer get "$control" | grep "Limits:" | tr -s ' ' | cut -d' ' -f4)"
-current="$(amixer get "$control" | sed -n '/dB/{p;q}' | sed 's/.*Playback \([0-9]*\) .*/\1/')"
-wjt -l "$lolimit" -u "$uplimit" -x "$current" | while read next; do
-    amixer -q set "$control" "$next"
-done
+wjt -l "$lolimit" -u "$uplimit" -x "$current" | while read next; do setV "$next"; done
